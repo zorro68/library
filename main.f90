839291495c0f04f59ab961e5f26c3de9a0f02fb6
David <davidzorro68@gmail.com>
2017-04-12 21:27:37 +0200

    PRUEBAS

--- main.f90
+++ main.f90
@@ -3,27 +3,21 @@
     PROGRAM LIB
     
     USE MOD_FACT
-    USE STRINGS
+    !USE MOD_STRINGS
+    
+    INTEGER(4) :: IRES, IVAR=8
+    INTEGER(8) :: IIRES, IIVAR=8
+    REAL(4) :: SRES, SVAR=8.0
+    REAL(8) :: DRES, DVAR=8.D0
+    REAL(16) :: QRES, QVAR=8.Q0
+    REAL(4) :: SSRES
+    REAL(8)::DSRES
+    REAL(16)::QSRES
     
     REAL*8 DVAL, DVAL1
     REAL*8 DGAMMA
     REAL*16 QVAL,QVAL1
-    
-    INTEGER(4) :: IRES, IVAR=23
-    INTEGER(8) :: IIRES, IIVAR=23
-    REAL(4) :: SRES, SVAR=23.0
-    REAL(8) :: DRES, DVAR=23.D0
-    REAL(16) :: QRES, QVAR=23.Q0
-    
-    
-    pause
         
-    DVAL=DGAMMA(1.2D0)
-    DVAL1=GAMMA(1.2D0)
-    
-    WRITE(*,*)DVAL
-    WRITE(*,*)DVAL1
-    
     IRES=FACT(IVAR)
     WRITE(*,*)"     ",IVAR,"    ",IRES
     IIRES=FACT(IIVAR)
@@ -34,6 +28,20 @@
     WRITE(*,*)PRECISION(DVAR),"     ",DVAR,"    ",DRES
     QRES=FACT(QVAR)
     WRITE(*,*)PRECISION(QVAR),"     ",QVAR,"    ",QRES
+    SSRES=SEMIFACT(SVAR)
+    WRITE(*,*)PRECISION(SVAR),"     ",SVAR,"    ",SSRES
+    DSRES=SEMIFACT(DVAR)
+    WRITE(*,*)PRECISION(DVAR),"     ",DVAR,"    ",DSRES
+    QSRES=SEMIFACT(QVAR)
+    WRITE(*,*)PRECISION(DVAR),"     ",QVAR,"    ",QSRES
+        
+    WRITE(*,*)'**********************************************************************'
+    
+    DVAL=DGAMMA(1.2D0)
+    DVAL1=GAMMA(1.2D0)
+    
+    WRITE(*,*)DVAL
+    WRITE(*,*)DVAL1
     
     PAUSE
     
